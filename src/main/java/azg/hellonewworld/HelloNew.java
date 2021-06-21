fb0bd89227043234e0ea21d1a09a670e44aaa422
Anu George <anukgt@gmail.com>
2021-06-20 22:01:14 -0400

    first Commit

---
+++ src/main/java/azg/hellonewworld/HelloNew.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package azg.hellonewworld;
+
+/**
+ *
+ * @author vinug
+ */
+public class HelloNew {
+  public static void main(String[] args) {
+      System.out.println("Hello New WONDERFUL WORLD");
+  } 
+}
